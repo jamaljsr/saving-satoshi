5eae0dd84ef34574232de8ac32b712289b2043ba
jamaljsr <1356600+jamaljsr@users.noreply.github.com>
2026-02-01 18:26:07 -0600

    e2e: add chapter 10 tests

--- e2e/pages/challenges/index.ts
+++ e2e/pages/challenges/index.ts
@@ -5,4 +5,7 @@ export { HashChallengePage } from './hash.challenge'
 export { HashRateChallengePage } from './hashrate.challenge'
 export { TerminalChallengePage } from './terminal.challenge'
 export { OpCodeChallengePage } from './opcode.challenge'
-export { TransactionsChallengePage } from './transactions.challenge'
+export {
+  TransactionsChallengePage,
+  type TransactionAnswer,
+} from './transactions.challenge'
